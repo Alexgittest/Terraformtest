73bc50cf2afcf459d7175ae3f627ce2cb7ba1499
Aliaksandr <39026425+Alexgittest@users.noreply.github.com>
2021-04-18 11:52:58 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,19 +3,26 @@ provider "aws" {
     region = "eu-central-1"
 }
 
-variable "ami_type" {
-  type = string
-  description = "Security group name"
-  default = "ami-042e8287309f5df03"
-}
 variable "ssh_key_name" {
   type = string
   description = "Security group name"
   default = "devops-training"
 }
+data "aws_ami" "my_ubuntu" {
+	  most_recent = true
+	  filter {
+	    name   = "name"
+	    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+	  }
+	  filter {
+	    name   = "virtualization-type"
+	    values = ["hvm"]
+	  }
+	  owners = ["099720109477"] 
+}
 
 resource "aws_instance" "docker_server"{
-    ami = var.ami_type
+    ami = "${data.aws_ami.my_ubuntu.id}"
     instance_type="t2.micro"
     vpc_security_group_ids = [aws_security_group.allow_ssh_and_5000.id]
     key_name=var.ssh_key_name
