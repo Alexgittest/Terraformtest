f4e35852301e3584090b1909dd2f1065d3a3ee6d
Aliaksandr <39026425+Alexgittest@users.noreply.github.com>
2021-04-18 11:25:49 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,12 +1,59 @@
-provider "aws" {
-    region = "us-east-1"
-}
-	
-resource "aws_vpc" "jenkinstest" {
-	cidr_block       = "10.0.0.0/16"
-	instance_tenancy = "default"
 
-	tags = {
-    Name = "jenkinstest"
+provider "aws" {
+    region = "eu-central-1"
+}
+
+variable "ami_type" {
+  type = string
+  description = "Security group name"
+  default = "ami-042e8287309f5df03"
+}
+variable "ssh_key_name" {
+  type = string
+  description = "Security group name"
+  default = "devops-training"
+}
+
+resource "aws_instance" "docker server"{
+    ami = var.ami_type
+    instance_type="t2.micro"
+    vpc_security_group_ids = [aws_security_group.allow_ssh_and_5000.id]
+    key_name=var.ssh_key_name
+    user_data = <<EOF
+#!/bin/bash
+apt update -y
+EOF
+}
+
+resource "aws_security_group" "allow_ssh_and_5000" {
+  name        = "allow_http_ssh"
+  description = "Allow HTTP inbound traffic"
+  #vpc_id      = aws_vpc.main.id
+
+  ingress {
+    description = "5000 from any"
+    from_port   = 5000
+    to_port     = 5000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "SSH from any"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["46.53.128.0/17"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "allow_ssh_and_5000"
   }
 }
