86738e39585024ccd4edbd344311431f98622eac
Aliaksandr <39026425+Alexgittest@users.noreply.github.com>
2021-04-18 12:12:49 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -8,17 +8,18 @@ variable "ssh_key_name" {
   description = "Security group name"
   default = "devops-training"
 }
-data "aws_ami" "my_ubuntu" {
-	  most_recent = true
-	  filter {
-	    name   = "name"
-	    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
-	  }
-	  filter {
-	    name   = "virtualization-type"
-	    values = ["hvm"]
-	  }
-	  owners = ["099720109477"] 
+
+data "aws_ami" "my_ubuntu" {
+    most_recent=true
+    filter {
+        name = "name"
+        values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+    }
+    filter {
+    name = "virtualization-type"
+    values = ["hvm"]
+    }
+    owners = ["099720109477"]
 }
 
 resource "aws_instance" "docker_server"{
